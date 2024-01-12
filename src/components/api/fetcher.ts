9a5211179bd1fa8562969f47639398b46629484c
konradkoza <kozakonrad72@gmail.com>
2024-01-12 10:45:26 +0100

    confirm

--- src/components/api/fetcher.ts
+++ src/components/api/fetcher.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     //zmienic ip na swoje
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: "http://10.128.144.169:8080/api/v1",
     headers: {
         'Content-Type': 'application/json',
     }
